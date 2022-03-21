d68239180183881a41730333708c025a3e2286a3
RaymundoH21 <71298292+RaymundoH21@users.noreply.github.com>
2022-03-21 10:48:53 -0700

    Update Examen Unidad 1 ray
    Se agrega mi parte de la practica evaluatoria que es la mitad faltante de la misma.

--- Unidad 1/Evaluation/ExamUnit1.scala
+++ Unidad 1/Evaluation/ExamUnit1.scala
@@ -35,3 +35,35 @@ net.describe().show()
 import org.apache.spark.sql.Column
 val nData = net.withColumn("HVRatio", net("High")/net("Volume"))
 nData.show() 
+
+//8-.¿Qué día tuvo el pico mas alto en la columna “Open”?
+val maxp = newData.orderBy(desc("Open"))
+maxp.select("Date").limit(1).show()
+
+//9-.¿Cuál es el significado de la columna Cerrar “Close” en el contexto de información financiera, expliquelo no hay que codificar nada?
+/*
+Cuando el precio de la columna High sube parece ser lo mismo para la columna Close solo que esta 
+siempre en menor cantidad que la High, lo que quiere decir que a medida que High
+sube lo mas probables es que Close tambien lo haga pero en menor cantidad*/
+
+//10-.¿Cuál es el máximo y mínimo de la columna “Volumen”?
+df.select(max("Volume")).show()
+df.select(min("Volume")).show()
+
+//11-.Con Sintaxis Scala/Spark $ conteste los siguiente:
+
+//a-.Cuantos dias fue la columna "Close" inferior a $600?
+df.filter($"Close"<600).count()
+
+//b-.Que porcentaje del tiempo fue la columna "High" mayor que $500?
+val tiempo:Double = df.filter($"High">500).count()
+val porcentaje:Double = (tiempo*100)/1259
+
+//c-.Cual es la correlacion de pearson entre columna "High" y la columna "Volumen"?
+df.select(corr("High","Volume").alias("Correlacion")).show()
+
+//d-.Cual es el maximo de la columna "High" por año?
+df.groupBy(year(df("Date")).alias("Year")).max("High").sort(asc("Year")).show()
+
+//e-.Cual es el promedio de la columna "Close" para cada mes del calendario?
+df.groupBy(month(df("Date")).alias("Month")).avg("Close").sort(asc("Month")).show()
