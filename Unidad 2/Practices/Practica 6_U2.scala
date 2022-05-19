94ca0d4645253a92aa7e2f58d36f23ab778212c2
paularamos18 <paula.ramos18@tectijuana.edu.mx>
2022-05-19 12:11:44 -0700

    Update Practica 6_U2

--- Unidad 2/Practices/Practica 6_U2.scala
+++ Unidad 2/Practices/Practica 6_U2.scala
@@ -1,34 +1,13 @@
-import org.apache.spark.mllib.classification.{SVMModel, SVMWithSGD}
-import org.apache.spark.mllib.evaluation.BinaryClassificationMetrics
-import org.apache.spark.mllib.util.MLUtils
+import org.apache.spark.ml.classification.LinearSVC
 
 // Load training data in LIBSVM format.
-val data = MLUtils.loadLibSVMFile(sc, "data/mllib/sample_libsvm_data.txt")
+val training  = spark.read.format("libsvm").load("C:/Spark/spark-2.4.8-bin-hadoop2.7/data/mllib/sample_libsvm_data.txt")
 
-// Split data into training (60%) and test (40%).
-val splits = data.randomSplit(Array(0.6, 0.4), seed = 11L)
-val training = splits(0).cache()
-val test = splits(1)
+//Set the maximum number of iterations and the regularization parameter 
+val lsvc = new LinearSVC().setMaxIter(10).setRegParam(0.1)
 
-// Run training algorithm to build the model
-val numIterations = 100
-val model = SVMWithSGD.train(training, numIterations)
+// Fit the model
+val lsvcModel = lsvc.fit(training)
 
-// Clear the default threshold.
-model.clearThreshold()
-
-// Compute raw scores on the test set.
-val scoreAndLabels = test.map { point =>
-  val score = model.predict(point.features)
-  (score, point.label)
-}
-
-// Get evaluation metrics.
-val metrics = new BinaryClassificationMetrics(scoreAndLabels)
-val auROC = metrics.areaUnderROC()
-
-println(s"Area under ROC = $auROC")
-
-// Save and load model
-model.save(sc, "target/tmp/scalaSVMWithSGDModel")
-val sameModel = SVMModel.load(sc, "target/tmp/scalaSVMWithSGDModel")
+// Print the coefficients and intercept for linear svc
+println(s"Coefficients: ${lsvcModel.coefficients} Intercept: ${lsvcModel.intercept}")
